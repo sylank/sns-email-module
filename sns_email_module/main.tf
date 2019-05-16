7234c2e7a2127e2fee539755d4ef3ba213452829
Daniel Serleg <daniel.serleg@bitrise.io>
2019-05-16 22:40:23 +0200

    Remove SQS queue

--- sns_email_module/main.tf
+++ sns_email_module/main.tf
@@ -2,16 +2,6 @@ resource "aws_sns_topic" "email_topic" {
   name = "${var.email_topic_name}"
 }
 
-resource "aws_sqs_queue" "letter_queue" {
-  name = "${var.letter_queue_name}"
-}
-
-resource "aws_sns_topic_subscription" "letter_queue_subscription" {
-  topic_arn = "${aws_sns_topic.email_topic.arn}"
-  protocol  = "sqs"
-  endpoint  = "${aws_sqs_queue.letter_queue.arn}"
-}
-
 resource "null_resource" "sns_subscribe" {
   depends_on = ["aws_sns_topic.email_topic"]
 
--- sns_email_module/variables.tf
+++ sns_email_module/variables.tf
@@ -2,10 +2,6 @@ variable "email_topic_name" {
   
 }
 
-variable "letter_queue_name" {
-  
-}
-
 variable "email_address_list" {
    type = "list"
 }
